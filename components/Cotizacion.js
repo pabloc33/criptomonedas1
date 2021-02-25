21c0777b073456b8b6bb0e6dd4500d759e25a196
pabloc33 <pablo.contino33@gmail.com>
2021-02-24 22:48:52 -0300

    Primer Commit

---
+++ components/Cotizacion.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Cotizacion = ({resultado}) => {
+  if (Object.keys(resultado).length === 0) return null;
+
+  return (
+    <View style={styles.resultado}>
+      <Text style={[styles.texto, styles.precio]}>
+        <Text style={styles.span}>{resultado.PRICE}</Text>
+      </Text>
+      <Text style={styles.texto}>
+        Precio más alto del día:{' '}
+        <Text style={styles.span}>{resultado.HIGHDAY}</Text>
+      </Text>
+      <Text style={styles.texto}>
+        Precio más bajo del día:{' '}
+        <Text style={styles.span}>{resultado.LOWDAY}</Text>
+      </Text>
+      <Text style={styles.texto}>
+        Variación última 24 horas:{' '}
+        <Text style={styles.span}>{resultado.CHANGEPCT24HOUR}</Text>
+      </Text>
+      <Text style={styles.texto}>
+        Última Actualización:{' '}
+        <Text style={styles.span}>{resultado.LASTUPDATE}</Text>
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  resultado: {
+    backgroundColor: '#5E49E2',
+    padding: 20,
+  },
+  texto: {
+    color: '#FFF',
+    fontFamily: 'Lato-Regular',
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  precio: {
+    color: '#FFF',
+    fontFamily: 'Lato-Regular',
+    fontSize: 38,
+    marginBottom: 10,
+  },
+  span: {
+    fontFamily: 'Lato-Black',
+  },
+});
+
+export default Cotizacion;
